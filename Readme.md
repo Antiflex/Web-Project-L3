afae2ad837b40edd5a28cb75a1cc76eb8043e814
ALexandre <68072873+Antiflex@users.noreply.github.com>
2023-09-15 20:53:17 -0700

    add Readme.md and begin writing the script for the tic-tac-toe game

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Web Project : EZ Games
+*features :*  
+a few simple games like tic-tac-toe or whack-a-hole
--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const express = require('express');
 console.log("Hello World");
+let g1Matrix = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]];
+
+function fillMatrix(coords, choice){
+    g1Matrix[coords[0]][coords[1]] = choice;
+}
