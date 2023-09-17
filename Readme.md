52f4910dd13deea0eeb115bdc2c5e087f344d870
ALexandre <68072873+Antiflex@users.noreply.github.com>
2023-09-16 18:39:02 -0700

    updated fillMatrix function and Readme.md

--- Readme.md
+++ Readme.md
@@ -1,3 +1,7 @@
 # Web Project : EZ Games
-*features :*  
-a few simple games like tic-tac-toe or whack-a-hole
+***main features :***  
+a few simple games like tic-tac-toe or whack-a-hole  
+a login system to have the scores saved
+
+***other features :***
+a leaderboard for each game
--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 console.log("Hello World");
-let g1Matrix = [
+
+// matrix for the tic-tac-toe game
+// 0 = empty square, 1 = filled with a cross, 2 = filled with a circle
+let ticTacToeMat = [
     [0,0,0],
     [0,0,0],
     [0,0,0]];
 
 function fillMatrix(coords, choice){
-    g1Matrix[coords[0]][coords[1]] = choice;
+    if (ticTacToeMat[coords[0]][coords[1]] === 0) {
+        ticTacToeMat[coords[0]][coords[1]] = choice;
+    }
+    else
+        throw new Error('place is already filled')
 }
+
+console.log(ticTacToeMat)
