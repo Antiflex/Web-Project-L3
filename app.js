07db09be93e7b253a318acf326e4b4af1f0ecec4
ALexandre <68072873+Antiflex@users.noreply.github.com>
2023-09-21 20:05:29 -0700

    - add static route and html files for tic-ta-toe and whack-a-mole - move index.html and made a basic page - add thumbnail images - rename script name to 'start' in package.json

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require('express');
+const fs = require('fs');
+const promisify = require('util')
+
 const app = express();
+app.use(express.static("public"));
+// public endpoints
 
-// routes
-
-app.get("/", (req, res) => {
-    console.log("Here");
-    res.status(500);
-    res.send("hi");
+app.get("/",(req, res) => {
+    res.status(500).sendFile("public/index.html",{ root: "./" });
 });
 
-app.get(["/index", "/static/index/index.html"],(req, res) => {
-    res.status(500).sendFile("static/index/index.html",{ root: "./" });
+app.get("/whack-a-mole",(req, res) => {
+    res.status(500).sendFile("public/whack_a_mole.html",{ root: "./" });
+});
+
+app.get("/tic-tac-toe",(req, res) => {
+    res.status(500).sendFile("public/tic_tac_toe.html",{ root: "./" });
 });
 
 app.all("*",(req, res) => {
     res.status(404).send('<h1>Error 404 : Page not found</h1>');
 });
 app.listen(3000,()=>{});
+
+// dynamic endpoints
