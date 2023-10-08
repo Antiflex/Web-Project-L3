8a3ac70609649d74be488a7e16ddc03a4eacd97d
ALexandre <68072873+Antiflex@users.noreply.github.com>
2023-10-07 21:03:20 -0700

    add migration file

---
+++ database/migrations/20231007180121_init.cjs
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createSchema('public', (table)=>{
+      table.increments('id_test');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('test');
+};
